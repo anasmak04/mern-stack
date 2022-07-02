15deac7bb666b8a014215f350995f9bbecc826aa
anasmak03 <anas11.makhloufi@gmail.com>
2022-07-02 17:41:02 +0100

    server backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const colors = require('colors')
+require('dotenv').config();
+const app = express();
+app.use(express.json());
 
-require('dotenv').config()
-const app = express()
 
-const Routes = require('./routes/router')
-
-app.use(express.json())
+const Routes = require('./routes/router');
 app.use('/api/routes',Routes);
+
+app.use((req,res, next) => {
+  console.log(req.path,req.method)
+  next()
+})
+
+/// mongodb
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+  console.log(`database connected`.green)
+})
+.catch((err) => {
+  console.log(err)
+})
+
 
 
 
   port = 8080
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`server run in ${port}`)
+  })
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../Models/UserModel')
 
 router.get('/', (req,res) => {
     res.json({msg : 'welcome to get method'})
 })
 
 router.get('/:id', (req,res) => {
-    res.json({msg : 'get all width id'})
+    res.json({msg : 'get all infos id'})
 })
 
-router.post('/', (req,res) => {
-    res.json({msg : 'enter a new msgs'})
+router.post('/', async (req,res) => {
+    const {name,email,msg} = req.body;
+    try{
+        const routers = await User.create({name,email,msg})
+        res.status(200).json(routers); 
+
+    }
+    catch(err){
+        console.log(err)
+    }
+    // res.json({msg : 'enter a new msgs'})
 })
 
 
@@ -26,6 +35,6 @@ router.patch('/:id', (req,res) => {
 
 
 
-module.exports = router
+module.exports = router;
 
 
