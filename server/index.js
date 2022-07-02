0fc43e787508b85a87d780312a0de1ece3a8b1ba
anasmak03 <anas11.makhloufi@gmail.com>
2022-07-02 16:06:23 +0100

    server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const colors = require("colors")
-require("dotenv").config()
-const app = express();
+const express = require('express')
+
+require('dotenv').config()
+const app = express()
 
 
-port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.json({msg : "hello my express app"})
+  })
 
+  port = 8080
 
-app.listen(() => {
-    console.log(`server run in port ${port}`.blue)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
